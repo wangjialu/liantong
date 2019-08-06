9b00507b18a5cdb3a890288b197249a8478781c8
wangjialu <962822886@qq.com>
2019-08-06 15:19:34 +0800

    测试

---
+++ src/liantong/test/Test.java
@@ -0,0 +1,12 @@
+package liantong.test;
+
+public class Test {
+    private static Test ourInstance = new Test();
+
+    public static Test getInstance() {
+        return ourInstance;
+    }
+
+    private Test() {
+    }
+}
